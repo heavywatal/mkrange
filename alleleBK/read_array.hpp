57909c87f1f8dfa6cefd092d8d14d6e20abc32e2
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2018-08-12 14:13:33 +0900

    :art: Refactor read_int_array()

--- alleleBK/read_array.hpp
+++ alleleBK/read_array.hpp
@@ -3,101 +3,42 @@
 #define READ_ARRAY_HPP_
 
 #include <fstream>
-#include <iostream>
+#include <sstream>
+#include <iterator>
 #include <string>
 #include <vector>
 
-#include <cerrno>
+namespace wtl {
 
-/////////1/////////2/////////3/////////4/////////5/////////6/////////7/////////8
-
-class Fios: public std::fstream{
-  public:
-	explicit Fios(
-			const std::string& filename, 
-			const std::ios::openmode mode = std::ios::in | std::ios::out | std::ios::binary
-            ) : std::fstream(filename.c_str(), mode)
-    {
-        if (this->fail()) {
-            std::cerr << filename << ": " << std::strerror(errno) << std::endl;
-            exit(errno);
-        }
-    };
-};
-
-class Fin: public Fios{
-  public:
-	explicit Fin(
-			const std::string& filename,
-			const std::ios::openmode mode = std::ios::binary
-			)
-    : Fios(filename, mode | std::ios::in){}
-	
-	std::basic_istream<char>& readline(std::string* buffer, const char delimiter='\n'){
-		return std::getline(*this, *buffer, delimiter);
-	}
-	
-	template<class T>
-	std::basic_istream<char>& readlines(T* lines, const char delimiter='\n'){
-		std::string line;
-		while (readline(&line, delimiter)) {
-			if (!line.empty()) lines->push_back(line);
-		}
-		return *this;
-	}
-	
-	std::basic_istream<char>& read(std::string* buffer, const char delimiter='\0'){
-		return readline(buffer, delimiter);
-	}
-	
-	std::string readline(const char delimiter='\n'){
-		std::string buffer;
-		readline(&buffer, delimiter);
-		return buffer;
-	}
-	
-	std::vector<std::string> readlines(const char delimiter='\n'){
-		std::vector<std::string> lines;
-		readlines(&lines, delimiter);
-		return lines;
-	}
-	
-	std::string read(const char delimiter='\0'){ return readline(delimiter); }
-};
-
-inline std::vector<std::string> split(const std::string& src, const std::string& delimiter=" \t\n") {
-    std::vector<std::string> dst;
-    for (size_t delim_pos=0, start=0; delim_pos!=src.npos;) {
-        delim_pos = src.find_first_of(delimiter, start);
-        const int substr_size(delim_pos - start);
-        if (substr_size) {dst.push_back(src.substr(start, substr_size));}
-        ++(start = delim_pos);
+inline std::vector<std::string> readlines(std::istream& ist) {
+    std::vector<std::string> lines;
+    std::string buffer;
+    while (std::getline(ist, buffer)) {
+        lines.push_back(buffer);
     }
-    return dst;
+    return lines;
 }
 
-inline std::vector<int> split_int(const std::string& src, const std::string& delimiter=" \t\n") {
-    std::vector<int> dst;
-    for (size_t delim_pos=0, start=0; delim_pos!=src.npos;) {
-        delim_pos = src.find_first_of(delimiter, start);
-        const int substr_size(delim_pos - start);
-        if (substr_size) {dst.push_back(std::atoi(src.substr(start, substr_size).c_str()));}
-        ++(start = delim_pos);
-    }
-    return dst;
+template <class T>
+inline std::vector<T> split(const std::string& src) {
+    std::stringstream sst(src);
+    return std::vector<T>(std::istream_iterator<T>(sst), std::istream_iterator<T>());
 }
+
+} // namespace wtl
 
 inline std::vector<std::vector<int> > read_int_array(const std::string& filename) {
-    std::vector<std::string> vs(Fin(filename).readlines());
-    const size_t n(vs.size());
-    std::vector<std::vector<int> > vvi(n);
-    for (unsigned int i=0; i<n; ++i) {
-        vvi[i] = split_int(vs[i]);
+    std::ifstream ifs(filename.c_str());
+    ifs.exceptions(std::ios_base::badbit);
+    std::vector<std::string> lines(wtl::readlines(ifs));
+    const size_t n = lines.size();
+    std::vector<std::vector<int> > output;
+    output.reserve(n);
+    for (size_t i=0; i<n; ++i) {
+        if (lines[i].empty()) continue;
+        output.push_back(wtl::split<int>(lines[i]));
     }
-    return vvi;
+    return output;
 }
-
-
-/////////1/////////2/////////3/////////4/////////5/////////6/////////7/////////8
 
 #endif /* READ_ARRAY_HPP_ */
